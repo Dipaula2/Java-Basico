a3f1866bd0b3691ddb80fb944403f839dd1f9419
Guilherme de Paula <78801502+Dipaula2@users.noreply.github.com>
2023-03-27 11:02:19 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,17 +9,9 @@ public class Main {
        
        // Declaração de variaveis
        // camel case
-       
-            //boolean
-            //int
-            //        double
-            //            string 
-            //             float
-         
-       
-       
-              //System.out.println(vBoolean);  <-- printf
-              //System.out.println("Variavel booleana: " + vBoolean 
+             
+               //System.out.println(vBoolean);  <-- printf
+               //System.out.println("Variavel booleana: " + vBoolean 
                //+ "\nVariavel inteira: " +vInteiro 
                //+ "\nVariavel double: " +vFlutuante 
                //+ "\nVariavel texto: " +vString 
@@ -42,19 +34,21 @@ public class Main {
        
         
        // Recebendo dados em JOtionoane
+       //-----------------------------------------
        //String nome = JOptionPane.showInputDialog("digite Seu Nome");
        // System.out.println("Nome:"+nome);
        // -----------------------------------------
        // Recebendo dados em JOtionoane com Janela
-       String nome = JOptionPane.showInputDialog("digite Seu Nome");
-       String idade = JOptionPane.showInputDialog("Digite Sua Idade");
-       JOptionPane.showMessageDialog(null,"O nome digitado é: " +nome
-       + "\n Idade :" + idade);
+       //------------------------------------------
+       //String nome = JOptionPane.showInputDialog("digite Seu Nome");
+       // String idade = JOptionPane.showInputDialog("Digite Sua Idade");
+       //JOptionPane.showMessageDialog(null,"O nome digitado é: " +nome
+       // + "\n Idade :" + idade);
        
        // Manipulando String para outras
        // -------------------------------------------------------
        //String idade =  JOptionPane.showInputDialog("digite Sua Idade");
-       // int idadeInteria = Integer.parseInt(idade);
+       //int idadeInteria = Integer.parseInt(idade);
        // ------------------------------------------------------
        //int idade2 = Integer.parseInt(JOptionPane.showInputDialog("digite Sua Idade 2"));
        //JOptionPane.showMessageDialog(null,"A idade 2 digitada é: " +idade2);
